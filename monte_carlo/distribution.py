06d48647dff03ce53b0fd955e35149e422711a8b
fade <halvederaser@gmail.com>
2021-05-14 18:01:22 -0400

    Very basic distribution stuff added as a placeholder and testing.

---
+++ monte_carlo/distribution.py
@@ -0,0 +1,31 @@
+# Placeholder for Distribution class.
+
+# Imports
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+class Distribution:
+
+    #def __init__(self, n_datasets = 1, dataset = dataset):
+        # Testing on just arrays
+        #self.dist_type = dist_type
+        #self.n_datasets = n_samples
+        #self.dataset = dataset
+
+    def __init__(self):
+        pass
+
+    def normal(dataset):
+        # Working on proper binwidths
+        binwidth = ((max(dataset) - min(dataset)) /
+            (round(math.sqrt(len(dataset)))))
+        
+        plot = sns.histplot(
+            dataset,
+            kde = True,
+            bins = (np.arange(min(dataset), max(dataset) + binwidth, binwidth)),
+            stat = "probability")
+        plt.show()
