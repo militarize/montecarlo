d30be9f7dc8dde8f30552a1443408d4055116f73
fade <halvederaser@gmail.com>
2021-05-15 13:51:34 -0400

    Added minimal documentation.

--- montecarlo/dice.py
+++ montecarlo/dice.py
@@ -2,14 +2,15 @@
 import numpy as np
 
 class Dice:
-    """
-    Really basic dice implementation that suffices for now.
-    """
-    def __init__(self, n_dice = 2):
-        self.n_dice = 2
-        self.results = []
+    """Really basic dice implementation that suffices for now."""
+
+    def __init__(self, n_dice = 1):
+        self.n_dice = n_dice            # of dice per throw
+        self.results = []               # results of all throws
+        #self.n_sides = n_sides         # of sides of each die
 
     def roll(self, n_rolls = 1):
+        """n_rolls of n_dice."""
         for roll in range(n_rolls):
             total = 0
             for i in range(self.n_dice):
@@ -17,4 +18,5 @@ class Dice:
             self.results.append(total)
 
     def get_results(self):
+        """Returns a list of all dice throws."""
         return self.results
