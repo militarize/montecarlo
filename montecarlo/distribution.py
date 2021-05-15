3700ede7904e1753cd2e0b4b8f227fe5bb63f4c9
fade <halvederaser@gmail.com>
2021-05-15 13:50:55 -0400

    Added minimal documentation.

--- montecarlo/distribution.py
+++ montecarlo/distribution.py
@@ -1,5 +1,3 @@
-# Placeholder for Distribution class.
-
 # Imports
 import math
 
@@ -8,7 +6,11 @@ import numpy as np
 import seaborn as sns
 
 class Distribution:
+    """The Distribution class placeholder.
 
+    This placeholder exists currently as a demo.
+    No plans on keeping this as a class.
+    """
     #def __init__(self, n_datasets = 1, dataset = dataset):
         # Testing on just arrays
         #self.dist_type = dist_type
@@ -19,11 +21,17 @@ class Distribution:
         pass
 
     def normal(dataset):
+        """Currently returns a matplotlib AxesSubplot.
+
+        Accepts single array or list for now.
+        """
+
         # Working on proper binwidths
         binwidth = ((max(dataset) - min(dataset)) /
             (round(math.sqrt(len(dataset)))))
         
-        # This plot can definitely be improved
+        # This plot can definitely be improved.
+        # The curve is not "smooth" enough.
         return sns.histplot(
             dataset,
             kde = True,
