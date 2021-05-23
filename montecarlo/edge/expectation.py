73e8b04bd2bd1be978551c92117126add98fa52a
fade <halvederaser@gmail.com>
2021-05-23 19:06:29 -0400

    Testing expectations.

---
+++ montecarlo/edge/expectation.py
@@ -0,0 +1,5 @@
+# expectation.py
+#   What did you expect?
+#   The expectation module is related to aspects of the wager itself.
+#   This includes calculations for expected value (EV) and returning of odds.
+#
