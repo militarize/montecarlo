20478df15bddc9658f2d75ca9ff3adbe551d14eb
fade <halvederaser@gmail.com>
2021-05-30 17:16:25 -0400

    Changed combo stuff and added n_combos/n_perms

--- montecarlo/edge/event.py
+++ montecarlo/edge/event.py
@@ -5,6 +5,7 @@
 
 # Imports
 import itertools
+import math
 
 
 def event_space(sample_space):
@@ -19,7 +20,7 @@ def event_space(sample_space):
     return itertools.chain.from_iterable(itertools.combinations(sample_space, r) for r in range(len(sample_space) + 1))
 
 
-def combos(sample_space, n, r = True, order = False):
+def combos(sample_space, n, repeat = True, order = False):
     """Uses a generator to return all combinations of given sample_space of n-length.
 
     Parameters:
@@ -30,12 +31,32 @@ def combos(sample_space, n, r = True, order = False):
     if order:
         for combo in itertools.product(sample_space, n):
             yield combo
-    elif r:
+    elif repeat:
         for combo in itertools.combinations_with_replacement(sample_space, n):
             yield combo
     else:
         for combo in itertools.combinations(sample_space, n):
             yield combo
+
+
+def n_combos(n, r, repeat = True):
+    """nCr: Returns number of combinations for choosing r items from n, repetition optional."""
+    f = math.factorial      # Alias math.factorial
+
+    if repeat:
+        return f(r + n - 1) // (f(r) * f(n - 1))
+    else:
+        return f(n) // (f(r) * f(n - r))
+
+
+def n_perms(n, r, repeat = True):
+    """nPr: Returns number of permutations for choosing r items from n, repetition optional."""
+    f = math.factorial
+
+    if repeat:
+        return n**r
+    else:
+        return f(n) // f(n - r)
 
 
 def self_select(sample_space, n):
@@ -71,7 +92,7 @@ def is_event(events, sample_space):
 
 
 def p_event(events, sample_space):
-    """Returns probability (real number) between 0 and 1 for given events in sample_space.
+    """Returns probability (real number) between 0 and 1 for given events in sample_space for a single trial.
 
     If sample_space == dict{}, we assume the values represent weights.
 
@@ -90,4 +111,4 @@ def p_event(events, sample_space):
     else:
         # Returns the count of all events in the sample_space,
         # divided by the total len of the sample_space.
-        return len([e for e in events if e in sample_space]) / len(sample_space)
+        return sum([1 for e in events if e in sample_space]) / len(sample_space)
